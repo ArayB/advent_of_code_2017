8a0a6d01ad9ca8bc808bb3fc62a2a5f42b92546b
Ant Barnes <ant@arayb.com>
2017-12-15 07:16:43 +0000

    Day fourteen part two solved

--- spec/day_fourteen/defrag_spec.rb
+++ spec/day_fourteen/defrag_spec.rb
@@ -2,16 +2,31 @@ require 'rspec'
 require_relative '../../lib/day_fourteen/defrag.rb'
 
 RSpec.describe DayFourteen::Defrag do
-  describe '#part_one'
-  it 'returns correctly' do
-    input = 'flqrgnkx'
+  describe '#part_one' do
+    it 'returns correctly' do
+      input = 'flqrgnkx'
 
-    expect(DayFourteen::Defrag.new.part_one(input)).to eq(8108)
+      expect(DayFourteen::Defrag.new.part_one(input)).to eq(8108)
+    end
+
+    it 'solves' do
+      input = 'wenycdww'
+
+      expect(DayFourteen::Defrag.new.part_one(input)).to eq(8226)
+    end
   end
 
-  it 'solves' do
-    input = 'wenycdww'
+  describe '#part_two' do
+    it 'returns correctly' do
+      input = 'flqrgnkx'
 
-    expect(DayFourteen::Defrag.new.part_one(input)).to eq(8226)
+      expect(DayFourteen::Defrag.new.part_two(input)).to eq(1242)
+    end
+
+    it 'solves' do
+      input = 'wenycdww'
+
+      expect(DayFourteen::Defrag.new.part_two(input)).to eq(1128)
+    end
   end
 end
