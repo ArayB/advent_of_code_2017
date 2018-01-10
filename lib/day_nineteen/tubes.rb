5b07497f3f7a8da0f1793766ff5ad7fe15b92bd4
Ant Barnes <anthony.barnes@thefloow.com>
2018-01-10 09:12:23 +0000

    Day nineteen part two

--- lib/day_nineteen/tubes.rb
+++ lib/day_nineteen/tubes.rb
@@ -1,14 +1,26 @@
 module DayNineteen
   class Tubes
     def part_one(input)
+      run(input)
+      @values.join
+    end
+
+    def part_two(input)
+      run(input)
+      @steps
+    end
+
+    private
+
+    def run(input)
       @grid = input.chomp.split("\n").map { |line| line.each_char.to_a }
 
       start_x = @grid[0].index('|')
       @position = { x: start_x, y: 0}
       @direction = :down
-
       @running = true
-      values = []
+      @values = []
+      @steps = 0
 
       while @running
         case current_val
@@ -20,18 +32,15 @@ module DayNineteen
           set_direction
           move
         else
-          values << current_val
+          @values << current_val
           check_finished
           move
         end
       end
-
-      values.join
     end
 
-    private
-
     def move
+      @steps += 1
       case @direction
       when :down
         @position[:y] += 1
@@ -71,18 +80,13 @@ module DayNineteen
     end
 
     def set_horizontal_direction
-      return @direction = :left if @position[:x] == @grid[@position[:y]].length - 1
-      return @direction = :right if value_right != ' '
+      return @direction = :right if value_right?
       @direction = :left
     end
 
     def set_vertical_direction
       return @direction = :up if value_above?
       @direction = :down
-    end
-
-    def value_right
-      @grid[@position[:y]][@position[:x] + 1]
     end
 
     def value_above?
--- spec/day_nineteen/tubes_spec.rb
+++ spec/day_nineteen/tubes_spec.rb
@@ -13,4 +13,16 @@ RSpec.describe DayNineteen::Tubes do
       expect(DayNineteen::Tubes.new.part_one(input)).to eq('NDWHOYRUEA')
     end
   end
+
+  describe '#part_two' do
+    it 'returns correctly' do
+      input = File.read('input/19/short.txt')
+      expect(DayNineteen::Tubes.new.part_two(input)).to eq(38)
+    end
+
+    it 'solves' do
+      input = File.read('input/19/input.txt')
+      expect(DayNineteen::Tubes.new.part_two(input)).to eq(17540)
+    end
+  end
 end
